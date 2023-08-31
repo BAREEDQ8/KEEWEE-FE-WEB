c35c6ff38168c34f79c358a8d6c8984b10ea9318
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-08-31 19:25:34 +0300

    slight adjustments to the sideBar

--- src/App.js
+++ src/App.js
@@ -36,28 +36,30 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <div className="app">
-            {/* <Sidebar isSidebar={isSidebar} /> */}
             <main className="content">
               <Topbar setIsSidebar={setIsSidebar} />
               {user ? (
-                <Routes>
-                  <Route path="/" element={<Dashboard />} />
-                  <Route path="/" element={<Sidebar />} />
-                  <Route path="/signin" element={<SignIn />} />
-                  <Route path="/team" element={<Team />} />
-                  <Route path="/contacts" element={<Contacts />} />
-                  {/* <Route path="/invoices" element={<Invoices />} /> */}
-                  <Route path="/form" element={<Form />} />
-                  <Route path="/bar" element={<Bar />} />
-                  <Route path="/pie" element={<Pie />} />
-                  <Route path="/line" element={<Line />} />
-                  <Route path="/faq" element={<FAQ />} />
-                  {/* <Route path="/calendar" element={<Calendar />} /> */}
-                  <Route path="/geography" element={<Geography />} />
-                </Routes>
+                <>
+                  <Sidebar isSidebar={isSidebar} />
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/team" element={<Team />} />
+                    <Route path="/contacts" element={<Contacts />} />
+                    {/* <Route path="/invoices" element={<Invoices />} /> */}
+                    <Route path="/form" element={<Form />} />
+                    <Route path="/bar" element={<Bar />} />
+                    <Route path="/pie" element={<Pie />} />
+                    <Route path="/line" element={<Line />} />
+                    <Route path="/faq" element={<FAQ />} />
+                    {/* <Route path="/calendar" element={<Calendar />} /> */}
+                    <Route path="/geography" element={<Geography />} />
+                  </Routes>
+                </>
               ) : (
                 <Routes>
                   <Route path="/" element={<SignIn />} />
+                  <Route path="/signin" element={<SignIn />} />
                 </Routes>
               )}
             </main>
